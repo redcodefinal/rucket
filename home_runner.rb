3ebe07c00b201818ab8dca043b2626fb70e1e0c5
Ian Rash <redcodefinal@gmail.com>
2017-01-28 15:38:56 -0800

    DHT11

--- home_runner.rb
+++ home_runner.rb
@@ -6,7 +6,7 @@ r.add_fan(:exhaust, 13)
 r.add_fan(:heatsink_fan, 19)
 r.add_fan(:intake, 26)
 
-r.add_module(Timer.new(r))
-r.add_module(DHT11Reader.new(r, 5))
+r.add_module(:timer, Timer.new(r))
+r.add_module(:dht, DHT11Reader.new(r, 5))
 
 r.run_loop
