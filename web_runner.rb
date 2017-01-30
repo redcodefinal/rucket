496a1593a1b969bf887f7baa689f434666bb06fd
Ian Rash <redcodefinal@gmail.com>
2017-01-30 09:30:24 -0800

    Sinatra

--- web_runner.rb
+++ web_runner.rb
@@ -2,17 +2,17 @@ require 'sinatra'
 require 'erb'
 require_relative "./lib/rucket"
 
-r = Rucket.new
-r.add_light(:main, 6)
-r.add_fan(:exhaust, 13)
-r.add_fan(:heatsink_fan, 19)
-r.add_fan(:intake, 26)
+$r = Rucket.new
+$r.add_light(:main, 6)
+$r.add_fan(:exhaust, 13)
+$r.add_fan(:heatsink_fan, 19)
+$r.add_fan(:intake, 26)
 
-r.add_module(:timer, Timer.new(r))
-r.add_module(:dht, DHT11Reader.new(r, 5))
+$r.add_module(:timer, Timer.new(r))
+$r.add_module(:dht, DHT11Reader.new(r, 5))
 
 get '/' do
-  r.update
+  $r.update
   erb :index
 end
 
@@ -25,7 +25,7 @@ __END__
     <meta http-equiv="refresh" content="5; URL=http://localhost:4567">
   </head>
   <body>
-    TEMP: <%= r.modules[:dht].temp %>F
+    TEMP: <%= $r.modules[:dht].temp %>F
     TIME: <%= Time.now %>
   </body>
 </html>
