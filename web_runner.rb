6d999e0ef8842406833671499d9a4132f3e5277c
Ian Rash <redcodefinal@gmail.com>
2017-01-30 09:31:25 -0800

    Sinatra

--- web_runner.rb
+++ web_runner.rb
@@ -8,8 +8,8 @@ $r.add_fan(:exhaust, 13)
 $r.add_fan(:heatsink_fan, 19)
 $r.add_fan(:intake, 26)
 
-$r.add_module(:timer, Timer.new(r))
-$r.add_module(:dht, DHT11Reader.new(r, 5))
+$r.add_module(:timer, Timer.new($r))
+$r.add_module(:dht, DHT11Reader.new($r, 5))
 
 get '/' do
   $r.update
