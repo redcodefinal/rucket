1248572789c43a9bca32221b8efbac0990b2b078
Ian Rash <redcodefinal@gmail.com>
2017-02-02 12:22:10 -0800

    DSL update

--- lib/rucket/fan.rb
+++ lib/rucket/fan.rb
@@ -7,17 +7,19 @@ class Fan
 
   def turn_on
     @pin.set_low
+    @on = true
   end
 
   def turn_off
     @pin.set_high
+    @on = false
   end
 
   def off?
-    @pin.low?
+    not @on
   end
 
   def on?
-    @pin.high?
+    @on
   end  
 end
--- lib/rucket/light.rb
+++ lib/rucket/light.rb
@@ -5,11 +5,21 @@ class Light
     @pin = GPIOPin.new(pin, :output)
   end
 
-  def turn_on
+def turn_on
     @pin.set_low
+    @on = true
   end
 
   def turn_off
     @pin.set_high
+    @on = false
   end
+
+  def off?
+    not @on
+  end
+
+  def on?
+    @on
+  end  
 end
