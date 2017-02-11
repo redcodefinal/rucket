a8c2ef3211afe9c1aad36c58f2a9ffb8a1de1b87
Ian Rash <redcodefinal@gmail.com>
2017-02-11 13:24:10 -0800

    Web App Charts

--- lib/rucket/modules/dht11_reader.rb
+++ lib/rucket/modules/dht11_reader.rb
@@ -1,17 +1,29 @@
-require "googlecharts"
+require "chartkick"
 require "dht-sensor-ffi"
 
 require_relative '../rucket_module'
 
 class DHT11Reader < RucketModule
   MAX_ENTRIES = 50
+  CHART_OPTIONS = {
+    xtitle: "Time",
+    ytitle: "Value",
+    legend: true,
+    height: "150px",
+    width: "400px",
+    colors: ["red", "blue"],
+    min: 0,
+    max: 100
+  }
   attr_reader :pin
   attr_reader :last_update
   attr_accessor :update_time
   attr_reader :temp, :humidity
+  attr_reader :temps, :humids
 
   def initialize(rucket, pin = 4, update_time = 60*30)
     super rucket
+
     @pin = pin
     @update_time = update_time
     @last_update = Time.now - update_time
@@ -21,25 +33,13 @@ class DHT11Reader < RucketModule
   def main_loop
     if (Time.now - last_update) > update_time
       @last_update = Time.now
-      @temps ||= []
-      @temps << (temp = DhtSensor.read(pin, 11).temp_f)
-      @temps.slice!(0) if @temps.count > MAX_ENTRIES
+      @temps ||= {}
+      @temps[Time.now] = (temp = DhtSensor.read(pin, 11).temp_f)
+      @temps.delete(@temps.first.first) if @temps.count > MAX_ENTRIES
 
-      @humids ||= []
-      @humids << (humid = DhtSensor.read(pin, 11).humidity)
-      @humids.slice!(0) if @humids.count > MAX_ENTRIES
-      
-      chart = Gchart.new(type: "line",
-                   title: "DHT",
-                   data: [@temps, @humids],
-                   size: "400x150",
-                   filename: "/home/pi/rucket/lib/rucket/public/chart.png",
-                   line_colors: 'ff0000,0000ff',
-                   legend: ["Tempurature", "Humidity"],
-                   axis_with_labels: ['Time', 'Value'],
-                   axis_labels: [0..30].to_a,
-                   max: 100)
-      chart.file
+      @humids ||= {}
+      @humids[Time.now] = (humid = DhtSensor.read(pin, 11).humidity)
+      @humids.delete(@humids.first.first) if @humids.count > MAX_ENTRIES
 
       @temp = temp
       @humidity = humid
