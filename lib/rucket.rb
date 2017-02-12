28ea25cdec50153b144af1637e5ff68d40261a1f
Ian Rash <redcodefinal@gmail.com>
2017-02-11 19:24:48 -0800

    Log Fixes

--- lib/rucket.rb
+++ lib/rucket.rb
@@ -9,7 +9,7 @@ require_rel "rucket/modules/*.rb"
 
 module Rucket
   extend self
-  FileUtils.mkdir_p(File.new(File.dirname(__FILE__) + '/log/'))
+  FileUtils.mkdir_p(File.dirname(__FILE__) + '/log/')
   LOG = Logger.new(File.new(File.dirname(__FILE__) + '/log/' + name + '.log', 'w'))
   LOG.info "Created log!"
 
